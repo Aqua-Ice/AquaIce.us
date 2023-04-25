cde43671b4f77aa21a2984dc07f4c2ac43c75747
Josiah <josiah@bloomer.cc>
2023-04-25 11:21:32 -0400

    Fix scrolling position

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
-let scrollButton = document.querySelector(`section.intro .btn`)
+let introSection = document.querySelector("section.intro")
+let header = document.querySelector("header")
+let scrollButton = introSection.querySelector(`.btn`)
+
+
 scrollButton.addEventListener("click", e => {
 	e.preventDefault()
 	window.scroll({
-		top: window.innerHeight,
+		top: introSection.offsetHeight - header.offsetHeight - 15,
 		behavior: "smooth",
 	})
 })
+
+function onScroll() {
+    console.log(window.scrollY)
+    if (window.scrollY >= 50) {
+        header.classList.add("active")
+    } else {
+        header.classList.remove("active")
+    }
+}
+window.addEventListener("scroll", onScroll)
+setInterval(onScroll, 1000)
